b57e6276425d4456a677b1a5113c1ddadc03cb33
Ravija Maheshwari <iamravija@yahoo.com>
2020-02-03 21:48:42 +0530

    Docker additions + minor frontend changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
-const server = app.listen(7000, () =>{
+const server = app.listen(8080, () =>{
   console.log(`Express running -> PORT ${server.address().port}`);
 });
 
